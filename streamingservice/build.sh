ce54e1800189fbf65626fdb2e3698c8fccbdcc20
Bhavya Thakker <2023mt93257@wilp.bits-pilani.ac.in>
2025-05-26 12:39:36 +0530

    Added StreamingService - It is responsible for getting the data from video service and stream it on RTSP
    Signed-off-by: Bhavya Thakker <2023mt93257@wilp.bits-pilani.ac.in>

---
+++ streamingservice/build.sh
@@ -0,0 +1,1 @@
+sudo docker build -t streamingservice:1.9.1 .
---
+++ streamingservice/run.sh
@@ -0,0 +1,2 @@
+sudo docker run -it --rm --network host  -p 8554:8554   -p 1935:1935   -p 8888:8888   -p 8889:8889   -p 8189:8189/udp   streamingservice:1.9.1
+#sudo docker run -it --rm --network host  -p 8554:8554   -p 1935:1935   -p 8888:8888   -p 8889:8889   -p 8189:8189/udp   my-mediamtx:1.9.1
